078d9d6c8ccd4151c63e5361a9263864e0bd7e41
otttisss <otttisss@yandex.ru>
2023-08-16 01:23:44 +0300

    OverTwoHoursOnEarth added | TODO: fill logic of classes and test on main class

---
+++ GridnineSystems/src/main/java/com/gridnine/testing/filtration/OverTwoHoursOnEarth.java
@@ -0,0 +1,12 @@
+package com.gridnine.testing.filtration;
+
+import com.gridnine.testing.model.Flight;
+
+import java.util.List;
+
+public class OverTwoHoursOnEarth implements FlightFilter {
+    @Override
+    public List<Flight> filterFlights(List<Flight> flights) {
+        return null;
+    }
+}
