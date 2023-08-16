47c27da04263b6a052c23ae00fa2e23f391ee0bf
otttisss <otttisss@yandex.ru>
2023-08-16 14:16:29 +0300

    Unit tests fixed

--- GridnineSystems/src/test/java/com/gridnine/testing/FlightFilterUnitTest.java
+++ GridnineSystems/src/test/java/com/gridnine/testing/FlightFilterUnitTest.java
@@ -1,28 +1,35 @@
 package com.gridnine.testing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FlightFilterUnitTest {
+    private List<Flight> flights;
+    @BeforeEach
+    void setUp() {
+        flights = FlightBuilder.createFlights();
+    }
+
     @Test
     void testDepartureBefore() {
         FlightFilter flightFilter = new DepartureBefore();
-        List<Flight> flights = flightFilter.filterFlights(FlightBuilder.createFlights());
-        assertEquals(5, flights.size());
+        List<Flight> filterFlights = flightFilter.filterFlights(flights);
+        assertEquals(5, filterFlights.size());
     }
 
     @Test
     void testArrivalBeforeDeparture() {
         FlightFilter flightFilter = new ArrivalBeforeDeparture();
-        List<Flight> flights = flightFilter.filterFlights(FlightBuilder.createFlights());
-        assertEquals(5, flights.size());
+        List<Flight> filterFlights = flightFilter.filterFlights(flights);
+        assertEquals(5, filterFlights.size());
     }
 
     @Test
     void testOverTwoHoursOnEarth() {
         FlightFilter flightFilter = new OverTwoHoursOnEarth();
-        List<Flight> flights = flightFilter.filterFlights(FlightBuilder.createFlights());
-        assertEquals(4, flights.size());
+        List<Flight> filterFlights = flightFilter.filterFlights(flights);
+        assertEquals(4, filterFlights.size());
     }
 }
